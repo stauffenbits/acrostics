6457d1bdbd73005040ad95352b35065c6981ad4d
Joshua M. Moore <thwee.abacadabra.alchemist@gmail.com>
2018-11-25 15:51:30 -0800

    next up, autocomplete

---
+++ app/js/acrostic-factory.component.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('acrosticApp')
+  .component('acrosticFactory', {
+    template: '<acrostic word="word" tabindex=0 words="words"></acrostic><button ng-click="$ctrl.download()">Download</button>',
+    controller: ['$scope', '$http', function($scope, $http){
+      this.download = function(){
+        download($('input').toArray().filter(i => $(i).val() ? $(i).val().length > 1 : false).map(i => $(i).val()).join('\n'), $scope.word + '.txt', 'text');
+      }
+
+      /*
+      this.words = [];
+      $http.get('/app/words.txt').then(response => {
+        this.words = response.data.split('\r\n');
+        // $('input').autocomplete({source: this.words, appendTo: 'input:focus'});
+      });
+      */
+    }],
+  })

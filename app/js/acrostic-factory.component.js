ef985491ddbbc6ccd6cd7bb3bd218735fb2fca9a
Joshua M. Moore <thwee.abacadabra.alchemist@gmail.com>
2019-03-10 18:25:09 -0700

    indented textfiles

--- app/js/acrostic-factory.component.js
+++ app/js/acrostic-factory.component.js
@@ -6,7 +6,15 @@ angular
     template: '<acrostic word="word" tabindex=0 words="words"></acrostic><button ng-click="$ctrl.download()">Download</button>',
     controller: ['$scope', '$http', function($scope, $http){
       this.download = function(){
-        download($('input').toArray().filter(i => $(i).val() ? $(i).val().length > 1 : false).map(i => $(i).val()).join('\n'), $scope.word + '.txt', 'text');
+        download(
+          $('input')
+          .toArray()
+          .filter(i => $(i).val() ? $(i).val().length > 1 : false)
+          .map(i => `${'  '.repeat(parseInt($(i).attr('tabindex'))}${$(i).val()}`)
+          .join('\n'),
+          $scope.word + '.txt', 
+          'text'
+        );
       }
 
       /*
